bbe3908594fe8f34bbd2a298a90dce552dcd0df3
wtao <wtao@macbook>
2024-05-04 09:24:33 -0700

    refine code

--- backend/query.mjs
+++ backend/query.mjs
@@ -1,23 +1,5 @@
 import { ObjectId } from "mongodb"; // to support new ObjectId()
 
-export const buildSubstitution = (req) => {
-  const getBucketCount = (from, to, intervalMs) => {
-    const current = new Date(from).getTime();
-    const toMs = new Date(to).getTime();
-    return (toMs - current) / intervalMs;
-  };
-
-  return {
-    $from: new Date(req.body.range.from),
-    $to: new Date(req.body.range.to),
-    $dateBucketCount: getBucketCount(
-      req.body.range.from,
-      req.body.range.to,
-      req.body.intervalMs
-    ),
-  };
-};
-
 export const parseQuery = (req, target) => {
   const {
     body: {
@@ -29,12 +11,30 @@ export const parseQuery = (req, target) => {
   const $from = new Date(from);
   const $to = new Date(to);
   const $intervalMs = intervalMs;
+  
   const getBucketCount = (from, to, intervalMs) => {
     const current = new Date(from).getTime();
     const toMs = new Date(to).getTime();
     return (toMs - current) / intervalMs;
   };
 
+  // expected query format
+  // const queryFromGrafana = {
+  //   collection: "collection name",
+  //   type: "table or timeserie",
+  //   aggregations: [
+  //     {
+  //       string: "string",
+  //       date: new Date(),
+  //       id: new ObjectId("573a1393f29313caabcdc50e"),
+  //       bool: true,
+  //       float: 12345.4,
+  //       expandable1: $from,
+  //       expandable2: $to,
+  //       expandable3: $intervalMs
+  //     },
+  //   ],
+  // };
   const jsObject = eval(`(${query})`);
   const { collection, aggregations } = jsObject;
   if (!collection || !aggregations) {
